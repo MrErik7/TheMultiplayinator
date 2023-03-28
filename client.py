9f16dce654dc114dd032ecb481d3c79a2b51ee63
erik-gustafsson-nti-johanneberg <42681756+MrErik7@users.noreply.github.com>
2023-03-28 10:29:01 +0200

    v1.3.1
    - Nu har allt fallerat, detta är väldigt svårt.
    - Ska byta till raspberry pi nu och börja jobba där istället.

--- client.py
+++ client.py
@@ -8,8 +8,8 @@ class Client:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # Initialize server info
-        self.ip = "192.168.10.183"
-        self.port = 38699
+        self.ip = "192.168.193.104"
+        self.port = 14576
 
     def connect(self):
         self.socket.connect((self.ip, self.port))
@@ -18,7 +18,7 @@ class Client:
         self.listen()
 
     def send_key_pressed(self, key):
-        self.socket.sendall(bytes(key.name, "utf-8"))
+        self.socket.sendall(bytes(str(key), "utf-8"))
 
     def listen(self):
         while True:
@@ -35,7 +35,20 @@ client = Client()
 client.connect()
 while True:
     try:
-        keyboard.on_press(client.send_key_pressed)
+
+        # https://www.circuitbasics.com/how-to-detect-keyboard-and-mouse-inputs-on-a-raspberry-pi/
+        if (keyboard.read_key() in "w"):
+            client.send_key_pressed("w")
+
+        elif (keyboard.read_key() in "a"):
+            client.send_key_pressed("a")
+
+        elif (keyboard.read_key() in "s"):
+            client.send_key_pressed("s")
+            
+        elif (keyboard.read_key() in "d"):
+            client.send_key_pressed("d")
+
         client.listen()
         
     except ConnectionResetError:
--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 # echo-server.py
 import socket
+import json
+import time
+
+# for debug
+import keyboard
 
 class Server:
     def __init__(self):
@@ -8,7 +13,39 @@ class Server:
 
         # Initialize server info
         self.ip = ""
-        self.port = 38699
+        self.port = 14576
+
+        # Initialize game state
+        self.game_state = {
+            "players": {
+                "player1": {
+                    "x": 0,
+                    "y": 0,
+                    "score": 0
+                },
+                "player2": {
+                    "x": 10,
+                    "y": 10,
+                    "score": 0
+                }
+            }
+        }
+
+    def update_game_state(self, player_id, x, y, score):
+        # Update the game state with the new player position and score
+        self.game_state["players"][player_id]["x"] = x
+        self.game_state["players"][player_id]["y"] = y
+        self.game_state["players"][player_id]["score"] = score
+
+
+    def send_game_state(self, conn):
+        # Convert the game state to JSON
+        game_state_json = json.dumps(self.game_state)
+
+        # Send the game state to the client
+        conn.sendall(game_state_json.encode())
+
+
 
     def start(self):
         # Bind socket to IP and port
@@ -34,6 +71,26 @@ class Server:
 
                     print(f"Received: {data}")
                     conn.sendall(b"Message received")
+
+                    # Now for the debugging part (remove this when script moves to retropie)
+                    decoded_data = data.decode()
+                    if "w" in decoded_data.lower():
+                        keyboard.press('w')
+
+                    if "a" in decoded_data.lower():
+                        keyboard.press('a')
+
+                    if "s" in decoded_data.lower():
+                        keyboard.press('s')
+
+                    if "d" in decoded_data.lower():
+                        keyboard.press('d')
+
+                    try:
+                        keyboard.release(decoded_data)
+                    except:
+                        print("Canty release")
+
 
 
                     if (data == "quit"):
