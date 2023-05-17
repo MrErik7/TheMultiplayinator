680024819b839a80123b3fee82f972b95accd3d9
erik-gustafsson-nti-johanneberg <42681756+MrErik7@users.noreply.github.com>
2023-05-17 14:10:56 +0200

    v2.1.1
    - La till piltangenter för svenskt tangentbord

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ class Client:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.ip = ip
         self.port = port
-        self.printable_keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', ',', '.', '/', ';', '\'', '[', ']', '\\', '-', '=', '`', 'space']
+        self.printable_keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', ',', '.', '/', ';', '\'', '[', ']', '\\', '-', '=', '`', 'space', 'down', 'up', 'left', 'right', 'uppil', 'vänsterpil', 'nedpil', 'högerpil']
 
         # Initialize callback function
         self.callback = None
@@ -31,8 +31,10 @@ class Client:
             try:                
                 # Check for key press and send it to server
                 event = keyboard.read_event()
+                print(event)
                 if event.event_type == "down" and event.name in self.printable_keys:
                     self.socket.sendall(("key:" + event.name).encode('utf-8'))
+                    
                 
                 elif event.event_type == "up" and event.name in self.printable_keys:
                     self.socket.sendall(("key_released:" + event.name).encode('utf-8'))
